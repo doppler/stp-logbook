3470a562187094f8c64cc985ec190f245cd63ef8
David Rose <doppler@gmail.com>
2018-11-09 06:14:10 -0500

    change App to a class component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import { store } from "react-recollect";
 
 import "./App.css";
 import StudentRouter from "./components/StudentRouter";
 
-const App = () => {
-  store.students = [];
-  store.filteredStudents = [];
-  store.filter = "";
-  return (
-    <div className="App">
-      <Route exact path="/" component={StudentRouter} />
-      <Route path="/student" component={StudentRouter} />
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    store.students = [];
+    store.filteredStudents = [];
+    store.filter = "";
+    return (
+      <div className="App">
+        <Route exact path="/" component={StudentRouter} />
+        <Route path="/student" component={StudentRouter} />
+      </div>
+    );
+  }
+}
 
 export default App;
 
