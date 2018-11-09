c5879f5a4edfaf234662fab13d09b7f816646d1c
David Rose <doppler@gmail.com>
2018-11-09 05:52:41 -0500

    set tabIndex of hidden submit button to -1

--- src/components/Jump.js
+++ src/components/Jump.js
@@ -188,7 +188,7 @@ export default collect(props => {
               <textarea id="notes" value={jump.notes} onChange={setAttribute} />
             </div>
           </fieldset>
-          <input type="submit" style={{ display: "hidden" }} />
+          <input type="submit" style={{ display: "hidden" }} tabIndex={-1} />
         </form>
       </div>
       <Footer
