fd3ca6be0f1599c1cf35b3bd38d3410e86840c12
David Rose <doppler@gmail.com>
2018-11-14 15:16:35 -0500

    Updates

---
+++ precache-manifest.273e20620d53bc4c73f9a76ae2b85d18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02822a67939591056d90",
+    "url": "/stp-logbook/static/css/main.cb0c038b.chunk.css"
+  },
+  {
+    "revision": "02822a67939591056d90",
+    "url": "/stp-logbook/static/js/main.02822a67.chunk.js"
+  },
+  {
+    "revision": "40fbca4f2261b053356b",
+    "url": "/stp-logbook/static/js/1.40fbca4f.chunk.js"
+  },
+  {
+    "revision": "787534b60dfc97140c2d",
+    "url": "/stp-logbook/static/js/runtime~main.787534b6.js"
+  },
+  {
+    "revision": "481c217443e6fda81de08d3c7f6d293e",
+    "url": "/stp-logbook/index.html"
+  }
+];
