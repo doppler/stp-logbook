3ea0d42e7aacaa8eb23d47848911bd7ca8723172
David Rose <doppler@gmail.com>
2018-11-15 10:56:33 -0500

    move selected phrases to ul#target

--- src/components/students/PhraseCloud.js
+++ src/components/students/PhraseCloud.js
@@ -13,14 +13,14 @@ const PhraseCloud = () => {
   };
 
   const onKeyDown = (keyName, e, handle) => {
-    const phraseCloudIsHidden = document
-      .querySelector("#PhraseCloud")
-      .classList.contains("hidden");
+    if (document.querySelector("#PhraseCloud").classList.contains("hidden"))
+      return null;
 
-    if (phraseCloudIsHidden) return null;
+    const target = document.querySelector("ul#target");
 
     let li = document.querySelector(`li[data-key="${keyName}"]`);
-    li.classList.toggle("selected");
+    // li.classList.toggle("selected");
+    target.appendChild(li);
   };
   return (
     <HotKeys
@@ -29,9 +29,10 @@ const PhraseCloud = () => {
         .map((_, i) => String.fromCharCode(i + 97))
         .join(",")}
     >
-      <div id="PhraseCloud" className={`hidden`}>
+      <div id="PhraseCloud" className={``}>
         <h2>{capitalize(phraseCloudKey)} Phrases</h2>
-        <ul>
+        <ul id="target" />
+        <ul id="source">
           {phraseCloud[phraseCloudKey].map((phrase, i) => (
             <li key={i} data-key={`${String.fromCharCode(i + 97)}`}>
               {phrase}
