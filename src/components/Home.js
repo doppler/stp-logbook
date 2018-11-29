749017157444e183e714dac15a55cbc8f4bc8e38
David Rose <doppler@gmail.com>
2018-11-29 15:51:40 -0500

    remove unneeded useState useEffect import

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import HotKeys from "react-hot-keys";
 import { store, collect } from "react-recollect";
 import Header from "./Header";
