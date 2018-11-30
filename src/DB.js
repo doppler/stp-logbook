2ab996c7824b65169c26ed9e5832de00823f8c93
David Rose <doppler@gmail.com>
2018-11-29 20:22:43 -0500

    hook up replication to url stored in localStorage

--- src/DB.js
+++ src/DB.js
@@ -4,15 +4,24 @@ PouchDB.plugin(PouchdbFind);
 
 const DB = new PouchDB("stp-logbook", { auto_compaction: true });
 
-const masterURL = "http://localhost:5984/stp-dev";
-const opts = { live: true, retry: true };
+(async () => {
+  const masterURL = localStorage.getItem("stp-logbook:couchDbUrl");
 
-DB.replicate.from(masterURL).on("complete", info => {
-  console.debug(`Replicating from ${masterURL}`, info);
-  DB.sync(masterURL, opts)
-    .on("change", change => console.debug("DB sync change", change))
-    .on("paused", console.debug("DB sync paused"))
-    .on("error", error => console.error(error));
-});
+  if (masterURL) {
+    const res = await fetch(masterURL);
+    const json = await res.json();
+    if (json.db_name) {
+      console.log(`Starting replication with ${masterURL}`);
+      const opts = { live: true, retry: true };
+
+      DB.replicate.from(masterURL).on("complete", info => {
+        DB.sync(masterURL, opts)
+          .on("change", change => console.debug("DB sync change", change))
+          .on("paused", console.debug("DB sync paused"))
+          .on("error", error => console.error(error));
+      });
+    }
+  }
+})();
 
 export default DB;
