6e1bac521086d7c1e042f31e3ec12779999facea
David Rose <doppler@gmail.com>
2018-12-05 10:44:25 -0500

    Add api endpoints for listing and displaying video content.

--- api/server.js
+++ api/server.js
@@ -1,12 +1,47 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
 const PORT = 4000;
 
 app.use(bodyParser.json());
+
+app.get("/api/videos", (req, res) => {
+  fs.readdir(process.env.STP_VIDEO_STORAGE_PATH, (err, items) => {
+    if (err) {
+      res.send({ error: err });
+      return false;
+    }
+    const output = items.map(item => item);
+    res.send(output);
+  });
+});
+
+app.get("/api/videos/:student_name", (req, res) => {
+  const { student_name } = req.params;
+  fs.readdir(
+    path.join(process.env.STP_VIDEO_STORAGE_PATH, student_name),
+    (err, items) => {
+      if (err) {
+        res.send({ error: err });
+        return false;
+      }
+      const output = items.map(item => item);
+      res.send(output);
+    }
+  );
+});
+
+app.get("/api/videos/:student_name/:video_filename", (req, res) => {
+  const { student_name, video_filename } = req.params;
+  res.sendFile(
+    path.join(process.env.STP_VIDEO_STORAGE_PATH, student_name, video_filename)
+  );
+});
 
 app.get("/", (req, res) => {
   console.log("GET /");
