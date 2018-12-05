752464e27db54c57da6e42a6abc240bafbdd60a5
David Rose <doppler@gmail.com>
2018-12-05 09:36:37 -0500

    remove unused server code

--- api/server.js
+++ api/server.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const fs = require("fs");
-
-const studentsDb = path.join(__dirname, "db", "students.json");
 
 const app = express();
 
@@ -11,48 +8,11 @@ const PORT = 4000;
 
 app.use(bodyParser.json());
 
-app.get("/api", (req, res) => res.json({ hello: "dook" }));
-
-app.get("/api/students", (req, res) => {
-  console.log("GET /api/students");
-  res.sendFile(studentsDb, {
-    headers: { "Content-Type": "application/json" }
-  });
-});
-
-app.post("/api/students", (req, res) => {
-  console.log("POST /api/students");
-  console.log("req", JSON.stringify(req.body));
-  const db = fs.createWriteStream(studentsDb);
-  db.write(JSON.stringify(req.body, undefined, 2));
-  db.end();
-  res.send(req.body);
-});
-
-app.post("/api/student", (req, res) => {
-  console.log("req", JSON.stringify(req.body));
-  const student = req.body;
-  const students = require(studentsDb);
-  const otherStudents = students.filter(obj => obj.id !== student.id);
-  const updatedStudents = [student, ...otherStudents];
-  const db = fs.createWriteStream(studentsDb);
-  db.write(JSON.stringify(updatedStudents, undefined, 2));
-  db.end();
-  res.send(student);
-});
-
-app.get("/api/instructors", (req, res) => {
-  res.send(
-    JSON.stringify(
-      ["", "David Rose", "Darcy King", "Cray Adamson", "Matteo Fialdini"].sort()
-    )
-  );
+app.get("/", (req, res) => {
+  console.log("GET /");
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
 app.use(express.static(path.join(__dirname, "..", "build")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
-});
 
 app.listen(PORT, () => console.log(`api-server listening on port ${PORT}`));
