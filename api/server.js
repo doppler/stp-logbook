1b940f3466ca0add70d4c82e04edef6ff1d9d386
David Rose <doppler@gmail.com>
2018-12-06 10:00:08 -0500

    create POST endpoint for uploading video

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
 const path = require("path");
 const fs = require("fs");
 
@@ -9,6 +10,8 @@ const app = express();
 const PORT = 4000;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(fileUpload());
 
 app.get("/api/videos", (req, res) => {
   fs.readdir(process.env.STP_VIDEO_STORAGE_PATH, (err, items) => {
@@ -41,6 +44,26 @@ app.get("/api/videos/:student_name/:video_filename", (req, res) => {
   res.sendFile(
     path.join(process.env.STP_VIDEO_STORAGE_PATH, student_name, video_filename)
   );
+});
+
+app.post("/api/videos/:student_id", (req, res) => {
+  const { student_id } = req.params;
+  const { video_filename } = req.body;
+  const studentVideoDir = path.join(
+    process.env.STP_VIDEO_STORAGE_PATH,
+    student_id
+  );
+  if (!fs.existsSync(studentVideoDir)) {
+    fs.mkdirSync(studentVideoDir);
+  }
+  const studentVideoPath = path.join(studentVideoDir, video_filename);
+  console.log(studentVideoPath);
+  console.log(req.files.file);
+  req.files.file.mv(studentVideoPath);
+  res.send({
+    ok: `/api/videos/${student_id}/${video_filename}`,
+    path: studentVideoPath
+  });
 });
 
 app.get("/", (req, res) => {
