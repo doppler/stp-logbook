8b2c723b280036650251891925d546e359743ea9
David Rose <doppler@gmail.com>
2018-12-10 09:51:12 -0500

    remove db change listener

--- src/DB.js
+++ src/DB.js
@@ -1,6 +1,5 @@
 import PouchDB from "pouchdb";
 import PouchdbFind from "pouchdb-find";
-import { store } from "react-recollect";
 PouchDB.plugin(PouchdbFind);
 
 const DB = new PouchDB("stp-logbook", { auto_compaction: true });
@@ -16,25 +15,9 @@ const DB = new PouchDB("stp-logbook", { auto_compaction: true });
       const opts = { live: true, retry: true };
 
       DB.replicate.from(masterURL).on("complete", info => {
-        DB.sync(masterURL, opts)
-          .on("change", info => handleDbChange(info))
-          .on("error", error => console.error(error));
+        DB.sync(masterURL, opts).on("error", error => console.error(error));
       });
     }
   }
 })();
-
-const handleDbChange = info => {
-  if (info.direction === "pull") {
-    info.change.docs.map(doc => {
-      if (doc.type === "student") {
-        const student = doc;
-        if (store.student && store.student._id === student._id) {
-          store.student = student;
-        }
-      }
-      return null;
-    });
-  }
-};
 export default DB;
