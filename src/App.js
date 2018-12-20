0a3daf94d30e338c44a5a8ac9d6ca34e950515bb
David Rose <doppler@gmail.com>
2018-12-19 23:56:30 -0500

    change App.js back to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import initializeDatabase from "./utils/initializeDatabase";
@@ -9,22 +9,18 @@ import StudentRouter from "./components/students/StudentRouter";
 import InstructorRouter from "./components/instructors/InstructorRouter";
 import AircraftRouter from "./components/aircraft/AircraftRouter";
 
-class App extends Component {
-  componentDidMount() {
-    initializeDatabase();
-  }
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Route exact path="/" component={Dashboard} />
-          <Route path="/students" component={StudentRouter} />
-          <Route path="/instructors" component={InstructorRouter} />
-          <Route path="/aircraft" component={AircraftRouter} />
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => {
+  useEffect(() => initializeDatabase(), []);
+  return (
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/students" component={StudentRouter} />
+        <Route path="/instructors" component={InstructorRouter} />
+        <Route path="/aircraft" component={AircraftRouter} />
+      </div>
+    </Router>
+  );
+};
 
 export default App;
